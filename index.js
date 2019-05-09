0a0aada71dd8df6313c87e7db15a1bae2d801a79
Jeremy Blong <jeremyblong@macbook-pro-3.home>
2019-05-09 13:25:21 -0400

    changed greeting

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send({ hi: "There" });
+	res.send({ hi: "My application is working!" });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log("Server is listening at port 5000");
+	console.log(`Server is running at port ${PORT}`);
 });
