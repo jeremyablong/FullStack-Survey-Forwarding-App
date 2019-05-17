8cf1bfbf0c09e66c38005d07c3b12182365ea817
Jeremy Blong <jeremyblong@macbook-pro-3.home>
2019-05-17 14:42:00 -0400

    removed keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // keys.js - figure out what set of credentials to return
 if (process.env.NODE_ENV === "production") {
 	// production - return prod set of keys
-	module.exports = require("./prod.js");
+	module.exports = require("./dev.js");
 } else {
 	// development return dev keys
 	module.exports = require("./dev.js");
