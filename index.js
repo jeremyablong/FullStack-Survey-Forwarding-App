272854c67c78a901c0486c72cefb8b311b3ae112
Jeremy Blong <jeremyblong@macbook-pro-3.home>
2019-05-25 16:33:02 -0400

    fourth commit correcting files

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   //
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendfile(path.join(__dirname = './client/build/index.html'));
   })
 }
 //build mode
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 
